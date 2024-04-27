5fc2911e600f3c3b20d0cb1aed10080c115e6731
Komjaunietis <132224969+Komjaunietis@users.noreply.github.com>
2024-04-27 21:25:55 +0300

    1.2.3

--- motivs.js
+++ motivs.js
@@ -1,24 +1,50 @@
-const poga = document.getElementById("mainit-motivu");
+//atbild par tumšā/gaišā motīva maiņu
+const mainitmotivu = document.getElementById("mainit-motivu");
 const labpratTumsi = window.matchMedia("(prefers-color-scheme: dark)");
 const motivsTagad = localStorage.getItem("motivs");
 if (motivsTagad == "tumsi") {
     document.body.classList.toggle("tumsi");
-    console.log("motivsTagad: tumsi")
 } else {
     document.body.classList.toggle("gaisi");
-    console.log("motivsTagad: gaisi")
 }
-poga.addEventListener("click", function (){
+mainitmotivu.addEventListener("click", function (){
+    document.body.classList.toggle("gaisi");
+    document.body.classList.toggle("tumsi");
     if (labpratTumsi.matches) {
-        document.body.classList.toggle("gaisi");
         var motivs = document.body.classList.contains("gaisi")
         ? "gaisi"
         : "tumsi";
     } else {
-        document.body.classList.toggle("tumsi");
         var motivs = document.body.classList.contains("tumsi")
         ? "tumsi"
         : "gaisi";
     }
     localStorage.setItem("motivs", motivs);
 });
+//atbild par raksta/grāmatas skata maiņu
+const mainitskatu = document.getElementById("mainit-skatu");
+const navgramata = document.querySelector('[notbook="true"]');
+const skatsTagad = localStorage.getItem("skats");
+if (navgramata == null) {
+    console.log("ir grāmata");
+    if (skatsTagad == "gramata") {
+        document.body.classList.toggle("gramata");
+    } else {
+        document.body.classList.toggle("raksts");
+    }
+    mainitskatu.addEventListener("click", function (){
+        document.body.classList.toggle("raksts");
+        document.body.classList.toggle("gramata");
+        if (document.body.classList.contains("raksts")) {
+            var skats = document.body.classList.contains("raksts")
+            ? "raksts"
+            : "tumsi";
+        } else {
+            var skats = document.body.classList.contains("gramata")
+            ? "gramata"
+            : "raksts";
+        }
+        localStorage.setItem("skats", skats);
+    });
+}
+else {console.log("nav grāmata");}
